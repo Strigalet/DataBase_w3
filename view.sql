a1d8f6734d4619ed209a4b1ad30b0e8c1aae857d
Bogdan Kasich <44712899+Strigalet@users.noreply.github.com>
2020-05-08 18:14:34 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -9,9 +9,3 @@ FROM Participant JOIN Award ON Participant.participant_id = Award.participant_id
 
 
 
-CREATE OR REPLACE VIEW Participant_Award AS
-    SELECT 
-        participant_id,
-        award_type,
-        year
-    FROM Award
